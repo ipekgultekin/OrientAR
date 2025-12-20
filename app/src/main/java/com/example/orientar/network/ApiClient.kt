5d3c3a4424ca68c8a8cb9f375162a51f27f57378
İpek Gültekin <ipek.gultekiin.01@gmail.com>
2025-12-20 11:20:53 +0200

    Update color resources

--- app/src/main/java/com/example/orientar/network/ApiClient.kt
+++ app/src/main/java/com/example/orientar/network/ApiClient.kt
@@ -1,15 +1,24 @@
 package com.example.orientar.network
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8000/"
+    private const val BASE_URL = "http://130.211.235.93:8000/"
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(120, TimeUnit.SECONDS)
+        .writeTimeout(120, TimeUnit.SECONDS)
+        .build()
 
     val api: ChatApi by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)   // 🔥 EN KRİTİK SATIR
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ChatApi::class.java)
